1ca42536d3285d6967aca51f615abf1cd1469a70
root <root@DESKTOP-8TOG95I.>
2025-08-28 17:05:41 +0000

    fix: set correct production redirectUri

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   const handleLogin = () => {
     const htiLaunchUrl = 'http://we-are-acc.vito.be/nl/hti/launch';
     const clientId = 'https://id.we-are-acc.vito.be/client/dcd2499f-656b-46ea-99ce-10aff48f1425';
-    const redirectUri = 'http://localhost:5176/';
+    const redirectUri = 'https://sage-cucurucho-4495c9.netlify.app/';
 
     const fullUrl = `${htiLaunchUrl}?client_id=${encodeURIComponent(clientId)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
 
